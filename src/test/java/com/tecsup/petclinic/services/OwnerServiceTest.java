d6069c1ae05ee328c2f847b61e9ba08633b5eb81
ismaeltec123 <ismael.quispe@tecsup.edu.pe>
2024-10-30 16:33:28 -0500

    findOwnerById

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,37 +1,52 @@
 package com.tecsup.petclinic.services;
 
+
+import com.tecsup.petclinic.entities.Pet;
+import com.tecsup.petclinic.exception.PetNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+import com.tecsup.petclinic.services.OwnerServiceImpl;
+import com.tecsup.petclinic.services.OwnerServiceImpl;
+import com.tecsup.petclinic.entities.Owner;
+import com.tecsup.petclinic.exception.OwnerNotFoundException;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
 
 @SpringBootTest
 @Slf4j
 public class OwnerServiceTest {
 
-	//@Autowired
-   		//private OwnerService ownerService;
+	@Autowired
+	private OwnerService ownerService;
 
-	/**
-	 * 
-	 */
-	/*
+
+
 	@Test
 	public void testFindOwnerById() {
 
-		long ID = 1;
-		String NAME = "Jaime";
+		String NAME_EXPECTED = "George";
+
+		Integer ID = 1;
+
 		Owner owner = null;
-		
+
 		try {
-			
-			owner = ownerService.findById(ID);
-			
-		} catch (OwnertNotFoundException e) {
+			owner = this.ownerService.findById(ID);
+		} catch ( OwnerNotFoundException e) {
 			fail(e.getMessage());
 		}
-		log.info("" + owner);
-
-		assertEquals(NAME, owner.getName());
-
+		assertEquals(NAME_EXPECTED, owner.getName());
 	}
-	*/
+
+
+
 }
