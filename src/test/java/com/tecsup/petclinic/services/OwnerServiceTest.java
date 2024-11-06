3e48624aa8e7c25be3e434619adb099774a6856a
ismaeltec123 <ismael.quispe@tecsup.edu.pe>
2024-11-06 13:15:56 -0500

    testOwnerFind

--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,18 +1,15 @@
 package com.tecsup.petclinic.services;
 
 
-import com.tecsup.petclinic.entities.Pet;
-import com.tecsup.petclinic.exception.PetNotFoundException;
+
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import static org.junit.jupiter.api.Assertions.fail;
-import com.tecsup.petclinic.services.OwnerServiceImpl;
-import com.tecsup.petclinic.services.OwnerServiceImpl;
+
 import com.tecsup.petclinic.entities.Owner;
 import com.tecsup.petclinic.exception.OwnerNotFoundException;
 
@@ -46,6 +43,44 @@ public class OwnerServiceTest {
 		}
 		assertEquals(NAME_EXPECTED, owner.getName());
 	}
+
+	@Test
+	public void testFindOwnerByFirstName() {
+
+		String FIND_FIRST_NAME = "George";
+		int SIZE_EXPECTED = 1;
+
+		List<Owner> owners = this.ownerService.findByFirstName(FIND_FIRST_NAME);
+
+		assertEquals(SIZE_EXPECTED, owners.size());
+	}
+
+	@Test
+	public void testFindOwnerByLastName() {
+
+		String FIND_LAST_NAME = "Franklin";
+		int SIZE_EXPECTED = 1;
+
+		List<Owner> owners = this.ownerService.findByLastName(FIND_LAST_NAME);
+
+		assertEquals(SIZE_EXPECTED, owners.size());
+	}
+
+	@Test
+	public void testFindOwnerByCity() {
+
+		String CITY = "McFarland";
+		int SIZE_EXPECTED = 1;
+
+		List<Owner> owners = this.ownerService.findByCity(CITY);
+
+		assertEquals(SIZE_EXPECTED, owners.size());
+	}
+
+
+
+
+
 
 
 
