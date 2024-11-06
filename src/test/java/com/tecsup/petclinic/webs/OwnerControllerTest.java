1e2d3256f7b65b987995131638c4c9423680bccb
BlackZhoul <alvaro.lluviians@gmail.com>
2024-11-06 15:15:57 -0500

    tercer commit

--- src/test/java/com/tecsup/petclinic/webs/OwnerControllerTest.java
+++ src/test/java/com/tecsup/petclinic/webs/OwnerControllerTest.java
@@ -97,4 +97,73 @@ public class OwnerControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    public void testDeleteOwnerKO() throws Exception {
+
+        mockMvc.perform(delete("/owners/1000"))
+                .andExpect(status().isNotFound());
+    }
+
+
+
+
+
+    @Test
+    public void testUpdateOwner() throws Exception {
+
+        String FIRST_NAME = "Clark";
+        String LAST_NAME = "Kent";
+        String ADDRESS = "789 Maple Lane";
+        String CITY = "Gotham";
+        String TELEPHONE = "1122334455";
+
+        String UP_FIRST_NAME = "Bruce";
+        String UP_LAST_NAME = "Wayne";
+        String UP_CITY = "Star City";
+
+        OwnerTO newOwnerTO = new OwnerTO();
+        newOwnerTO.setFirstName(FIRST_NAME);
+        newOwnerTO.setLastName(LAST_NAME);
+        newOwnerTO.setAddress(ADDRESS);
+        newOwnerTO.setCity(CITY);
+        newOwnerTO.setTelephone(TELEPHONE);
+
+        // CREATE
+        ResultActions mvcActions = mockMvc.perform(post("/owners")
+                        .content(om.writeValueAsString(newOwnerTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
+        String response = mvcActions.andReturn().getResponse().getContentAsString();
+        Integer id = JsonPath.parse(response).read("$.id");
+
+        // UPDATE
+        OwnerTO upOwnerTO = new OwnerTO();
+        upOwnerTO.setId(id);
+        upOwnerTO.setFirstName(UP_FIRST_NAME);
+        upOwnerTO.setLastName(UP_LAST_NAME);
+        upOwnerTO.setCity(UP_CITY);
+
+        mockMvc.perform(put("/owners/" + id)
+                        .content(om.writeValueAsString(upOwnerTO))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        // FIND
+        mockMvc.perform(get("/owners/" + id))
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(id)))
+                .andExpect(jsonPath("$.firstName", is(UP_FIRST_NAME)))
+                .andExpect(jsonPath("$.lastName", is(UP_LAST_NAME)))
+                .andExpect(jsonPath("$.city", is(UP_CITY)));
+
+        // DELETE
+        mockMvc.perform(delete("/owners/" + id))
+                .andExpect(status().isOk());
+    }
+
 }
